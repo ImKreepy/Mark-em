afd575dc1941e01c2929db6ac490fd3ec5b3a517
Im Kreepy <jkoscinski3@hotmail.com>
2026-04-30 14:58:53 -0400

    Moved CfgAmmo to its own HPP

---
+++ addons/main/CfgAmmo.hpp
@@ -0,0 +1,66 @@
+
+class CfgAmmo {
+    class SmokeShell;
+    class SmokeShellRed: SmokeShell {
+        mmf_isSignalMarker = 1;
+        mmf_markerColor = "ColorRed";
+        mmf_markerType = "mmf_SmokeMarker";
+    };
+    class SmokeShellGreen: SmokeShell {
+        mmf_isSignalMarker = 1;
+        mmf_markerColor = "ColorGreen";
+        mmf_markerType = "mmf_SmokeMarker";
+    };
+    class SmokeShellYellow: SmokeShell {
+        mmf_isSignalMarker = 1;
+        mmf_markerColor = "ColorYellow";
+        mmf_markerType = "mmf_SmokeMarker";
+    };
+    class SmokeShellPurple: SmokeShell {
+        mmf_isSignalMarker = 1;
+        mmf_markerColor = "ColorCiv";
+        mmf_markerType = "mmf_SmokeMarker";
+    };
+    class SmokeShellBlue: SmokeShell {
+        mmf_isSignalMarker = 1;
+        mmf_markerColor = "ColorBlue";
+        mmf_markerType = "mmf_SmokeMarker";
+    };
+    class SmokeShellOrange: SmokeShell {
+        mmf_isSignalMarker = 1;
+        mmf_markerColor = "ColorOrange";
+        mmf_markerType = "mmf_SmokeMarker";
+    };
+    
+    class G_40mm_Smoke;
+    class G_40mm_SmokeRed: G_40mm_Smoke {
+        mmf_isSignalMarker = 1;
+        mmf_markerColor = "ColorRed";
+        mmf_markerType = "mmf_SmokeMarker";
+    };
+    class G_40mm_SmokeGreen: G_40mm_Smoke {
+        mmf_isSignalMarker = 1;
+        mmf_markerColor = "ColorGreen";
+        mmf_markerType = "mmf_SmokeMarker";
+    };
+    class G_40mm_SmokeYellow: G_40mm_Smoke {
+        mmf_isSignalMarker = 1;
+        mmf_markerColor = "ColorYellow";
+        mmf_markerType = "mmf_SmokeMarker";
+    };
+    class G_40mm_SmokePurple: G_40mm_Smoke {
+        mmf_isSignalMarker = 1;
+        mmf_markerColor = "ColorCiv";
+        mmf_markerType = "mmf_SmokeMarker";
+    };
+    class G_40mm_SmokeBlue: G_40mm_Smoke {
+        mmf_isSignalMarker = 1;
+        mmf_markerColor = "ColorBlue";
+        mmf_markerType = "mmf_SmokeMarker";
+    };
+    class G_40mm_SmokeOrange: G_40mm_Smoke {
+        mmf_isSignalMarker = 1;
+        mmf_markerColor = "ColorOrange";
+        mmf_markerType = "mmf_SmokeMarker";
+    };
+};
--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -69,6 +69,7 @@ class CfgMods {
 };
 
 #include "CfgEventHandlers.hpp"
+#include "CfgAmmo.hpp"
 
 class CfgMarkers {
     class Flag;
@@ -85,71 +86,5 @@ class CfgMarkers {
         name = "Light Flare";
         icon = QPATHTOF(data\mmf_flare_light.paa);
         texture = QPATHTOF(data\mmf_flare_light.paa);
-    };
-};
-
-class CfgAmmo {
-    class SmokeShell;
-    class SmokeShellRed: SmokeShell {
-        mmf_isSignalMarker = 1;
-        mmf_markerColor = "ColorRed";
-        mmf_markerType = "mmf_SmokeMarker";
-    };
-    class SmokeShellGreen: SmokeShell {
-        mmf_isSignalMarker = 1;
-        mmf_markerColor = "ColorGreen";
-        mmf_markerType = "mmf_SmokeMarker";
-    };
-    class SmokeShellYellow: SmokeShell {
-        mmf_isSignalMarker = 1;
-        mmf_markerColor = "ColorYellow";
-        mmf_markerType = "mmf_SmokeMarker";
-    };
-    class SmokeShellPurple: SmokeShell {
-        mmf_isSignalMarker = 1;
-        mmf_markerColor = "ColorCiv";
-        mmf_markerType = "mmf_SmokeMarker";
-    };
-    class SmokeShellBlue: SmokeShell {
-        mmf_isSignalMarker = 1;
-        mmf_markerColor = "ColorBlue";
-        mmf_markerType = "mmf_SmokeMarker";
-    };
-    class SmokeShellOrange: SmokeShell {
-        mmf_isSignalMarker = 1;
-        mmf_markerColor = "ColorOrange";
-        mmf_markerType = "mmf_SmokeMarker";
-    };
-    
-    class G_40mm_Smoke;
-    class G_40mm_SmokeRed: G_40mm_Smoke {
-        mmf_isSignalMarker = 1;
-        mmf_markerColor = "ColorRed";
-        mmf_markerType = "mmf_SmokeMarker";
-    };
-    class G_40mm_SmokeGreen: G_40mm_Smoke {
-        mmf_isSignalMarker = 1;
-        mmf_markerColor = "ColorGreen";
-        mmf_markerType = "mmf_SmokeMarker";
-    };
-    class G_40mm_SmokeYellow: G_40mm_Smoke {
-        mmf_isSignalMarker = 1;
-        mmf_markerColor = "ColorYellow";
-        mmf_markerType = "mmf_SmokeMarker";
-    };
-    class G_40mm_SmokePurple: G_40mm_Smoke {
-        mmf_isSignalMarker = 1;
-        mmf_markerColor = "ColorCiv";
-        mmf_markerType = "mmf_SmokeMarker";
-    };
-    class G_40mm_SmokeBlue: G_40mm_Smoke {
-        mmf_isSignalMarker = 1;
-        mmf_markerColor = "ColorBlue";
-        mmf_markerType = "mmf_SmokeMarker";
-    };
-    class G_40mm_SmokeOrange: G_40mm_Smoke {
-        mmf_isSignalMarker = 1;
-        mmf_markerColor = "ColorOrange";
-        mmf_markerType = "mmf_SmokeMarker";
     };
 };

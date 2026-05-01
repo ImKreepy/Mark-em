ec71172d50bcee875cb6e670a968f64b3b9b9b1b
Im Kreepy <jkoscinski3@hotmail.com>
2026-05-01 14:53:49 -0400

    Bump MINOR version to 2 and BUILD number to 3 for new features and improvements

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,4 +1,4 @@
 #define MAJOR 1// Major Update, used for massive changes
-#define MINOR 1// Minor Update, used for new features and improvements
+#define MINOR 2// Minor Update, used for new features and improvements
 #define PATCH 0// Patch level, used for bug fixes and minor improvements
-#define BUILD 2// Build number, adds all versions together for a unique build number
+#define BUILD 3// Build number, adds all versions together for a unique build number
